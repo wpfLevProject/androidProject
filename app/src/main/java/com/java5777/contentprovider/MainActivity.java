1d528d9110b861de27f9b2162923b5940db01fe9
הודי קוניה <975195@gmail.com>
2017-04-16 14:02:23 +0300

    firstUpload

---
+++ app/src/main/java/com/java5777/contentprovider/MainActivity.java
@@ -0,0 +1,13 @@
+package com.java5777.contentprovider;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
